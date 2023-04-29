97e6de70973d23264f8f9d9d6e7f8382eada610c
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-04-29 22:07:40 +0600

    lets go!

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../shared/Header";
+
+const Home = () => {
+  return (
+    <div className="h-screen">
+      <Header />
+    </div>
+  );
+};
+
+export default Home;

47fc28ada4181171ee2562c66e8d4555c0c3fbb6
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-04-30 11:37:11 +0600

    add carousle

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,21 +1,59 @@
-import React from "react";
 import slide1 from "../../assets/Banner.png";
 import slide2 from "../../assets/Sreemongol.png";
 import slide3 from "../../assets/sundorbon.png";
+
+import React from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+
+import "./styles.css";
+
+// import required modules
+
 const Banner = () => {
   return (
-    <div className="carousel carousel-center max-w-md p-4 space-x-4 bg-neutral rounded-box">
-      <div className="carousel-item">
-        <img src={slide1} className="rounded-box" />
+    <div className="grid grid-cols-12 pl-[10%] items-center">
+      <div className="col-span-4 text-white">
+        <h1 className="text-5xl font-semibold uppercase">Cox's bazar</h1>
+        <p>
+          Cox's Bazar is a city, fishing port, tourism centre and district
+          headquarters in southeastern Bangladesh. It is famous mostly for its
+          long natural sandy beach, and it ...
+        </p>
+        <button>Booking</button>
       </div>
-      <div className="carousel-item">
-        <img src={slide2} className="rounded-box" />
-      </div>
-      <div className="carousel-item">
-        <img src={slide3} className="rounded-box" />
-      </div>
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        freeMode={true}
+        pagination={{
+          clickable: true,
+        }}
+        // modules={[FreeMode, Pagination]}
+        className="mySwiper col-span-8"
+      >
+        <SwiperSlide>
+          <img src={slide1} alt="" className="rounded-[20px]" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide2} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide3} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide3} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide3} alt="" />
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };
-
 export default Banner;
